ecf8a81fa9be2838b6b6c04e5072df1056a443c7
maykonmichel <maykonmichelpalma@gmail.com>
2020-12-29 10:10:44 -0300

    feat: evaluate

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import re
 import time
 
+import numpy as np
 import tensorflow as tf
 import unicodedata
 from sklearn.model_selection import train_test_split
@@ -264,3 +265,44 @@ for epoch in range(EPOCHS):
     print('Epoch {} Loss {:.4f}'.format(epoch + 1,
                                         total_loss / steps_per_epoch))
     print('Time taken for 1 epoch {} sec\n'.format(time.time() - start))
+
+
+def evaluate(sentence):
+    attention_plot = np.zeros((max_length_targ, max_length_inp))
+
+    sentence = preprocess_sentence(sentence)
+
+    inputs = [inp_lang.word_index[i] for i in sentence.split(' ')]
+    inputs = tf.keras.preprocessing.sequence.pad_sequences([inputs],
+                                                           maxlen=max_length_inp,
+                                                           padding='post')
+    inputs = tf.convert_to_tensor(inputs)
+
+    result = ''
+
+    hidden = [tf.zeros((1, units))]
+    enc_out, enc_hidden = encoder(inputs, hidden)
+
+    dec_hidden = enc_hidden
+    dec_input = tf.expand_dims([targ_lang.word_index['<start>']], 0)
+
+    for t in range(max_length_targ):
+        predictions, dec_hidden, attention_weights = decoder(dec_input,
+                                                             dec_hidden,
+                                                             enc_out)
+
+        # armazenar os pesos de atenção para traçar mais tarde
+        attention_weights = tf.reshape(attention_weights, (-1,))
+        attention_plot[t] = attention_weights.numpy()
+
+        predicted_id = tf.argmax(predictions[0]).numpy()
+
+        result += targ_lang.index_word[predicted_id] + ' '
+
+        if targ_lang.index_word[predicted_id] == '<end>':
+            return result, sentence, attention_plot
+
+        # o ID previsto é realimentado no modelo
+        dec_input = tf.expand_dims([predicted_id], 0)
+
+    return result, sentence, attention_plot
