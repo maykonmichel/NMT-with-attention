751be4587606236eb21a42d79e7db85cb1af7891
maykonmichel <maykonmichelpalma@gmail.com>
2020-12-29 10:03:03 -0300

    feat: attention layer

--- main.py
+++ main.py
@@ -113,3 +113,34 @@ class Encoder(tf.keras.Model):
 encoder = Encoder(vocab_inp_size, embedding_dim, units, BATCH_SIZE)
 
 
+class BahdanauAttention(tf.keras.layers.Layer):
+    def __init__(self, units):
+        super(BahdanauAttention, self).__init__()
+        self.W1 = tf.keras.layers.Dense(units)
+        self.W2 = tf.keras.layers.Dense(units)
+        self.V = tf.keras.layers.Dense(1)
+
+    def call(self, query, values):
+        # consulta forma de estado oculto == (batch_size, tamanho oculto)
+        # query_with_time_axis shape == (batch_size, 1, tamanho oculto)
+        # valores forma == (batch_size, max_len, tamanho oculto)
+        # estamos fazendo isso para transmitir a adição ao longo do eixo do tempo para calcular a pontuação
+        query_with_time_axis = tf.expand_dims(query, 1)
+
+        # forma de pontuação == (batch_size, max_length, 1)
+        # obtemos 1 no último eixo porque estamos aplicando pontuação a self.V
+        # a forma do tensor antes de aplicar self.V é (batch_size, max_length, unidades)
+        score = self.V(tf.nn.tanh(
+            self.W1(query_with_time_axis) + self.W2(values)))
+
+        # attention_weights forma == (batch_size, max_length, 1)
+        attention_weights = tf.nn.softmax(score, axis=1)
+
+        # forma do context_vector após soma == (batch_size, hidden_size)
+        context_vector = attention_weights * values
+        context_vector = tf.reduce_sum(context_vector, axis=1)
+
+        return context_vector, attention_weights
+
+
+attention_layer = BahdanauAttention(10)
