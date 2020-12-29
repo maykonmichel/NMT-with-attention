9ac4ee4e9817ddbcca27cecf645c4819c037c44d
maykonmichel <maykonmichelpalma@gmail.com>
2020-12-29 09:47:39 -0300

    feat: converts the unicode file to ascii

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import unicodedata
+
 path_to_file = "./por-eng/por.txt"
+
+
+# Converte o arquivo Unicode para ASCII
+def unicode_to_ascii(s):
+    return ''.join(c for c in unicodedata.normalize('NFD', s)
+                   if unicodedata.category(c) != 'Mn')
